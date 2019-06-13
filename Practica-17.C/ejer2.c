2bae37510de1aaf5de0a4e43e07ad80b6bccea3d
pdelam01 <pdelam01@estudiantes.unileon.es>
2019-06-13 10:46:20 +0200

    Archivos innecesarios eliminados

--- Practica-17.C/ejer2.c
+++
@@ -1,9 +0,0 @@
-void funcion (int matriz[100][100], int num1, int num2){
-	int t, aux;
-	for(t=0; t<100; t++){
-		aux=matriz[num1][t];
-		matriz[num1][t]=matriz[num2][t];
-		matriz[num2][t]=aux;
-	}
-	return;
-}
